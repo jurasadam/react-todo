921f61635f469b40a150843283893dda989de773
Adam Juras <hi@adamjuras.com>
2017-04-03 20:28:04 +0200

    Added form for new todos with tests

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,27 @@
+var React = require('react')
+
+var AddTodo = React.createClass({
+  onSubmit: function (e) {
+    e.preventDefault()
+    var todoText = this.refs.todoText.value
+
+    if (typeof todoText === 'string' && todoText.length > 0) {
+      this.refs.todoText.value = ''
+      this.props.onAddNewTodo(todoText)
+    } else {
+      this.refs.todoText.focus()
+    }
+  },
+  render: function () {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input type='text' placeholder='Start typing todo title ...' ref='todoText' />
+          <button className='button expanded'>Add New Todo</button>
+        </form>
+      </div>
+    )
+  }
+})
+
+module.exports = AddTodo
