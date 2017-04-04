5f0c6cb7b86f3d42bc011adb21c12884ce26e1a0
Adam Juras <hi@adamjuras.com>
2017-04-04 20:08:32 +0200

    Wired up search

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,32 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : []
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted
+    })
+
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var todoText = todo.text.toLowerCase()
+      return todoText.indexOf(searchText) >= 0 || searchText.length === 0
+    })
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1
+      } else if (a.completed && !b.completed) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+
+    return filteredTodos
   }
 }
