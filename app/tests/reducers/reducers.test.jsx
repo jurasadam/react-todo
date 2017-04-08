21692a5b9dad286ccebe66010e06f654bc50f9f5
Adam Juras <hi@adamjuras.com>
2017-04-08 12:34:33 +0200

    Added todo reducer with tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,37 @@ describe('Reducers', () => {
       expect(res).toEqual(true)
     })
   })
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      }
+      var res = reducers.todosReducer(df([]), df(action))
+
+      expect(res.length).toEqual(1)
+      expect(res[0].text).toEqual(action.text)
+    })
+  })
+
+  it('should toggle todo', () => {
+    var todosArray = [{
+      id: '1',
+      text: 'Walk the dog',
+      completed: true,
+      createdAt: 123,
+      completedAt: 125
+    }]
+
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: '1'
+    }
+
+    var res = reducers.todosReducer(df(todosArray), df(action))
+
+    expect(res[0].completed).toEqual(false)
+    expect(res[0].completedAt).toEqual(undefined)
+  })
 })
