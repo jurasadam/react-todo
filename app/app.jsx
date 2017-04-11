a49c80d76bd3c70f6b1793efe5e80838c0cf19e6
Adam Juras <hi@adamjuras.com>
2017-04-11 21:23:34 +0200

    Added Login & Logout actions

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,10 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var {Provider} = require('react-redux')
+var {Route, Router, IndexRoute, hashHistory} = require('react-router')
 
-var TodoApp = require('TodoApp')
+import TodoApp from 'TodoApp'
+import Login from 'Login'
 
 var actions = require('actions')
 var store = require('configureStore').configure()
@@ -16,7 +18,12 @@ require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp />
+    <Router history={hashHistory}>
+      <Route path='/'>
+        <Route path='todos' component={TodoApp} />
+        <IndexRoute component={Login} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 )
