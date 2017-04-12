e6591684679d1c776156cc81bf7d89fb5e54a094
Adam Juras <hi@adamjuras.com>
2017-04-12 07:26:56 +0200

    Add auth reducer and actions with tests

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -145,4 +145,25 @@ describe('Actions', () => {
       }, done())
     })
   })
+
+  describe('Auth tests', () => {
+    it('should generate login action object', () => {
+      const action = {
+        type: 'LOGIN',
+        uid: '123abc'
+      }
+      const res = actions.login(action.uid)
+
+      expect(res).toEqual(action)
+    })
+
+    it('should generate logout action object', () => {
+      const action = {
+        type: 'LOGOUT'
+      }
+      const res = actions.logout()
+
+      expect(res).toEqual(action)
+    })
+  })
 })
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -92,3 +92,27 @@ describe('Reducers', () => {
     expect(res[0]).toEqual(todos[0])
   })
 })
+
+describe('authReducer', () => {
+  it('should set uid on LOGIN', () => {
+    const action = {
+      type: 'LOGIN',
+      uid: '123'
+    }
+    const res = reducers.authReducer(undefined, df(action))
+
+    expect(res).toEqual({uid: action.uid})
+  })
+
+  it('should unset uid on LOGOUT', () => {
+    const authData = {
+      uid: '123abc'
+    }
+    const action = {
+      type: 'LOGOUT'
+    }
+    const res = reducers.authReducer(df(authData), df(action))
+
+    expect(res).toEqual({})
+  })
+})
